a0758afa78c6a9cb0d728abfd45dd09a03bb322e
Aditya Singh Ghosh <89689829+Adii1707@users.noreply.github.com>
2023-09-09 22:28:29 +0530

    ETA-App-Backend

---
+++ index.js
@@ -0,0 +1,29 @@
+// const jsonServer = require("json-server");
+// const server = jsonServer.create();
+// const router = jsonServer.router("db.json");
+// const middleware  = jsonServer.defaults();
+// const port = 4500;
+
+// server.use(middleware);
+// server.use(router);
+
+// server.listen(port);
+
+const jsonServer = require('json-server')
+const cors = require('cors')
+const path = require('path')
+
+const server = jsonServer.create()
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults()
+
+server.use(cors())
+server.use(jsonServer.bodyParser)
+server.use(middlewares)
+server.use(router)
+
+const PORT = 8000;
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`)
+})
